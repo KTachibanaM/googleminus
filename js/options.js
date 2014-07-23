278f294802be2477adc87c7847c8bdc0ed881366
KTachibanaM <whj19931115@gmail.com>
2014-07-23 20:06:34 +0800

    Implement remove

--- js/options.js
+++ js/options.js
@@ -27,9 +27,34 @@ function init_keywords_table() {
  * @param keyword {String}
  */
 function insert_a_row(i, keyword) {
+    // Insert the row
     var new_row = keywords_table.insertRow(i);
+
+    // Insert keyword cell
     var keyword_cell = new_row.insertCell(0);
     keyword_cell.innerHTML = keyword;
+
+    // Insert remove cell
+    var remove_cell = new_row.insertCell(1);
+    remove_cell.onclick = function () {
+        remove_keyword(keyword);
+        remove_a_row(keyword);
+    };
+    remove_cell.innerHTML = "<a href=''>X</a>";
+}
+
+/**
+ * Remove a row from keywords table
+ * @param keyword {String}
+ */
+function remove_a_row(keyword) {
+    var all_rows = keywords_table.rows;
+    for (var i = 0 ; i < all_rows.length ; ++i) {
+        if (all_rows[i].cells[0].innerText === keyword) {
+            keywords_table.deleteRow(i);
+            return;
+        }
+    }
 }
 
 init_keywords_table();
--- js/persistent.js
+++ js/persistent.js
@@ -11,14 +11,33 @@ function get_keywords() {
 
 /**
  * Add a new keyword in keywords
- *
  * @param {String} new_keyword
  */
 function add_keyword(new_keyword) {
     init_keywords();
     var keywords = localStorage.getItem(KEYWORDS_KEY);
     keywords = JSON.parse(keywords);
+
     keywords.push(new_keyword);
+
+    keywords = JSON.stringify(keywords);
+    localStorage.setItem(KEYWORDS_KEY, keywords);
+}
+
+/**
+ * Remove a keyword from keywords
+ * @param {String} removed_keyword
+ */
+function remove_keyword(removed_keyword) {
+    init_keywords();
+    var keywords = localStorage.getItem(KEYWORDS_KEY);
+    keywords = JSON.parse(keywords);
+
+    var index = keywords.indexOf(removed_keyword);
+    if (index > -1) {
+        keywords.splice(index, 1);
+    }
+
     keywords = JSON.stringify(keywords);
     localStorage.setItem(KEYWORDS_KEY, keywords);
 }
