2e7da9f7a1af57e3226ce294c22986d91c55a452
KTachibanaM <whj19931115@gmail.com>
2014-08-02 20:39:05 +0800

    Implement new version of keywords schema in main and google minus

--- js/googleminus.js
+++ js/googleminus.js
@@ -17,7 +17,7 @@ function KeywordConfig(keyword, filtering_mode, param) {
     this.param = param
 }
 
-const FILTERING_MODES = ["all_out", "selective_out", "blacken_keywords", "auto_ignore"];
+const FILTERING_MODES = ["all_out", "blacken_keywords"];
 const DEFAULT_FILTERING_MODE = "all_out";
 
 /**
@@ -45,48 +45,54 @@ function my_contains(str1, str2) {
  * Whether a post div contains keyword
  * @param   {Array} keywords
  * @param   {HTMLDivElement} post_div
- * @returns {boolean}
+ * @returns {String} keyword being caught
  */
 function scrutinize_post_div(keywords, post_div) {
 	for (var i = 0 ; i < keywords.length ; ++i) {
 		var keyword = keywords[i];
 		if (my_contains(post_div.outerText, keyword)) {
-			return true;
+			return keyword;
 		}
 	}
-	return false;
+	return null;
 }
 
 /**
  * Wipe the post div out of stream completely
  * @param {HTMLDivElement} post_div
+ * @param {String} keyword
+ * @param {String} param
  */
-function all_out(post_div) {
-    post_div.innerHTML = "<div>Filtered</div>";
+function all_out(post_div, keyword, param) {
+    post_div.innerHTML = "<div>Filtered " + build_black_cover(keyword) + "</div>";
 }
 
 /**
  * Replace keyword with black square in post div
  * @param {HTMLDivElement} post_div
- * @param {Array} keywords
+ * @param {Array} keyword
+ * @param {String} param
  */
-function blacken_keywords(post_div, keywords) {
-    for (var i = 0 ; i < keywords.length ; ++i) {
-        // Get keyword and build regex
-        var the_keyword = keywords[i],
-            regex = new RegExp(the_keyword, "g");
+function blacken_keywords(post_div, keyword, param) {
+    var regex = new RegExp(keyword, "g");
+    var replacement = (param === "") ? build_black_cover(keyword) : param;
 
-        // Build replacement with the same length
-        var replacement = new Array(the_keyword.length + 1).join("█");
-        replacement = "<span title='" + the_keyword + "'>" + replacement + "</span>";
-
-        // Replace the keyword
-        var raw_html = post_div.innerHTML;
-        post_div.innerHTML = raw_html.replace(regex, replacement);
-    }
+    // Replace the keyword
+    var raw_html = post_div.innerHTML;
+    post_div.innerHTML = raw_html.replace(regex, replacement);
 }
 
-function auto_ignore(post_div) {
+/**
+ * Build a span with keyword blacked out but still visible when on hover
+ * @param {String} keyword
+ * @returns {string} raw html for the span
+ */
+function build_black_cover(keyword) {
+    var replacement = new Array(keyword.length + 1).join("█");
+    return "<span title='" + keyword + "'>" + replacement + "</span>";
+}
+
+function auto_ignore(post_div, keyword, param) {
     // TODO: simulate click
     var options_span = post_div.getElementsByClassName(POST_OPTIONS_SPAN_CLASS_NAME)[0];
     options_span.click();
@@ -102,9 +108,9 @@ const POST_OPTIONS_SPAN_CLASS_NAME = "d-s xw if";
 
 /**
  * Main function
- * @param {Array} keywords
+ * @param {Array} keyword_configs
  */
-function filter(keywords) {
+function filter(keyword_configs) {
     // Find stream div
     var stream_div = document.getElementsByClassName(STREAM_DIV_CLASS_NAME)[0];
 
@@ -118,5 +124,19 @@ function filter(keywords) {
     my_append(all_post_divs, on_hover_post_divs);
 
     // Filter
-
+    var keyword_set = keyword_configs.map(function(config) {
+        return config.keyword;
+    });
+    all_post_divs.forEach(function (post_div) {
+        var keyword_caught = scrutinize_post_div(keyword_set, post_div);
+        if (keyword_caught !== null) {
+            var config_caught = keyword_configs.filter(function(config) {
+                return config.keyword === keyword_caught;
+            })[0];
+            var filtering_mode_caught = config_caught.filtering_mode;
+            var param_caught = config_caught.param;
+            console.log("[googleminus] Caught " + keyword_caught + ", executing mode " + filtering_mode_caught);
+            window[filtering_mode_caught](post_div, keyword_caught, param_caught);
+        }
+    })
 }
--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@ const interval = 300;
 
 chrome.runtime.sendMessage({method: "getPersistent"}, function(response) {
     var keywords = response.keywords;
-    console.log("---- Greetings from Google minus!");
-    console.log("---- Keywords are: ");
-    for (var i = 0 ; i < keywords.length ; ++i) {
-        var keyword_config = keywords[i];
-        console.log(keyword_config.keyword + " with mode " + keyword_config.filtering_mode + " with param " + keyword_config.param);
-    }
-    console.log("---- Keywords end");
+    console.log("[googleminus] Greetings from Google minus!");
+    console.log("[googleminus] Keywords are: ");
+    keywords.forEach(function(o) {
+        console.log(o.keyword + ", mode " + o.filtering_mode + ", param " + o.param);
+    });
+    console.log("[googleminus] Keywords end");
 
     chrome.extension.sendMessage({}, function(response) {
-        console.log("---- Start filtering with interval " + interval);
+        console.log("[googleminus] Start filtering with interval " + interval);
         var readyStateCheckInterval = setInterval(function() {
             if (document.readyState === "complete") {
                 filter(keywords);
