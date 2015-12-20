217af6d41cbaaf355c843e803fc09d6f6491c75d
KTachibanaM <whj19931115@gmail.com>
2015-12-20 13:21:11 -0600

    onChange

--- js/options.jsx
+++ js/options.jsx
@@ -1,20 +1,32 @@
 var KeywordRow = React.createClass({
+    handleParamChange: function (e) {
+        var new_param = e.target.value;
+        this.props.changeHandler(this.props.config.keyword, this.props.config.filtering_mode, new_param);
+    },
+    handleFilteringModeChange: function (e) {
+        var new_filtering_mode = e.target.value;
+        this.props.changeHandler(this.props.config.keyword, new_filtering_mode, this.props.config.param);
+    },
     render: function () {
         var filtering_mode_options = FILTERING_MODES.map(function (filtering_mode) {
             return (
-                <option value={filtering_mode}>{filtering_mode}</option>
+                <option key={filtering_mode} value={filtering_mode}>{filtering_mode}</option>
             );
         });
         return (
             <tr>
                 <td>{this.props.config.keyword}</td>
                 <td>
-                    <select value={this.props.config.filtering_mode}>
+                    <select
+                        defaultValue={this.props.config.filtering_mode}
+                        onChange={this.handleFilteringModeChange}>
                         {filtering_mode_options}
                     </select>
                 </td>
                 <td>
-                    <input type="text" default={this.props.config.param}/>
+                    <input type="text"
+                           defaultValue={this.props.config.param}
+                           onChange={this.handleParamChange}/>
                 </td>
                 <td>
                     <button>Delete</button>
@@ -30,19 +42,27 @@ var KeywordTable = React.createClass({
             configs: this.props.configs
         };
     },
+    handleChange: function (keyword, filtering_mode, param) {
+        modify_keyword_config(keyword, filtering_mode, param);
+    },
     render: function () {
+        var rootContext = this;
         var rows = this.state.configs.map(function (config) {
-            return <KeywordRow config={config}/>
+            return <KeywordRow key={config.keyword} config={config} changeHandler={rootContext.handleChange}/>
         });
         return (
             <table>
-                <tr>
-                    <th>Keyword</th>
-                    <th>Filtering mode</th>
-                    <th>Parameter</th>
-                    <th>Action</th>
-                </tr>
-                {rows}
+                <thead>
+                    <tr>
+                        <th>Keyword</th>
+                        <th>Filtering mode</th>
+                        <th>Parameter</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
             </table>
         )
     }
