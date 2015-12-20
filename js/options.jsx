1a49cf24dcb3f80e2d86201a6111bea95be0b516
KTachibanaM <whj19931115@gmail.com>
2015-12-20 12:02:48 -0600

    table

--- js/options.jsx
+++ js/options.jsx
@@ -1,12 +1,42 @@
-var KeywordLine = React.createClass({
-	render: function () {
-		return (
-			<div>This is a keyword line!!</div>
-		)
-	}
+var KeywordRow = React.createClass({
+    render: function () {
+        return (
+            <tr>
+                <td>{this.props.config.keyword}</td>
+                <td>{this.props.config.filtering_mode}</td>
+                <td>{this.props.config.param}</td>
+            </tr>
+        )
+    }
 });
 
+var KeywordTable = React.createClass({
+    getInitialState: function () {
+        return {
+            configs: this.props.configs
+        };
+    },
+    render: function () {
+        var lines = this.state.configs.map(function (config) {
+            return <KeywordRow config={config}/>
+        });
+        return (
+            <table>
+                <tr>
+                    <th>Keyword</th>
+                    <th>Filtering mode</th>
+                    <th>Parameter</th>
+                    <th>Action</th>
+                </tr>
+                {lines}
+            </table>
+        )
+    }
+});
+
+var configs = get_keyword_configs();
+
 ReactDOM.render(
-	<KeywordLine></KeywordLine>,
-	document.getElementById('app')
+    <KeywordTable configs={configs}/>,
+    document.getElementById('app')
 );
