9af5338abde9d8c1ae2bb92309e4cc6a03195654
KTachibanaM <whj19931115@gmail.com>
2015-12-20 11:39:19 -0600

    KeywordLine

--- js/options.jsx
+++ js/options.jsx
@@ -1,4 +1,12 @@
+var KeywordLine = React.createClass({
+	render: function () {
+		return (
+			<div>This is a keyword line!!</div>
+		)
+	}
+});
+
 ReactDOM.render(
-	<h1>Hello, world!</h1>,
+	<KeywordLine></KeywordLine>,
 	document.getElementById('app')
 );
